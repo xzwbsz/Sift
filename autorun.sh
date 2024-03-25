625f480baa057f02f852d5ac8543dd9cd5286df1
ZW.Xu <44642002+xzwbsz@users.noreply.github.com>
2024-03-25 15:25:36 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -5,4 +5,5 @@
   --num_servers 2 \
   --part_config ogbn_arxiv2part_data/ogbn-arxiv.json \
   --ip_config ip_config.txt \
-  "/home/gnn/miniconda3/envs/dgl/bin/python /home/gnn/workplace/hanjzTEST/dglexp/nn/gat_o.py"
+  "/home/gnn/miniconda3/envs/dgl/bin/python /home/gnn/workplace/hanjzTEST/dglexp/nn/gcn.py"
+  ## here using gat or sage to replace gcn can test other benchmark
